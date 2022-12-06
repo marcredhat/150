f26de473cb127b55b8789c48b9c7c7693b2ccdeb
Marc Chisinevski <marcgcp6@gmail.com>
2022-12-05 20:38:54 -0800

    Create cmltls.sh

---
+++ cmltls.sh
@@ -0,0 +1,32 @@
+
+
+ cat /opt/cloudera/CMCA/CMCA/private/.ca_key_password
+  
+ cat > tmp.py  << EOF
+import hashlib
+from subprocess import Popen, PIPE, STDOUT
+def hex_str(byteArr):
+ return ''.join(format(ord(x), '02x') for x in byteArr)
+obfuscated_password ='e298a3f09f92a3e298a0 1 Zglpbged7QHSzeJnPveIjclBk7l7nR1uAqOXXNoPpZ2 //k2XHb3l26V5eSYWbeWdBIC0nqWmaR1fUOOn6tJNTPRxyVoHQPjAycEYA=='
+hdr, version, random, enc = obfuscated_password.split()
+OBFUSCATION_SECRET="f09f998ff09f93bff09fa49e"
+OBFUSCATION_HEADER="e298a3f09f92a3e298a0"
+m=hashlib.sha512()
+m.update(random)
+m.update(OBFUSCATION_SECRET)
+m2 = m.copy()
+m.update("IV")
+iv = m.digest()[:16] # 128 bits for IV
+m2.update("KEY")
+key = m2.digest()[:32] # 256 bits for key
+p = Popen(["openssl", "enc", "-d", "-aes-256-ofb",
+ "-K", hex_str(key),
+ "-iv", hex_str(iv),
+ "-a", "-A",
+ ],
+ stdin=PIPE, stdout=PIPE, stderr=PIPE)
+stdout, stderr = p.communicate(enc)
+print (stdout)
+EOF
+ 
+ export host=ml-5fc768ab-a4a.apps.apps.airgap-l1-4-10.kcloud.cloudera.com
