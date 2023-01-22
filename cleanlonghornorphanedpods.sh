b5719143c281d71b16e0fa54b22124a0555e1663
Marc Chisinevski <marcgcp6@gmail.com>
2023-01-22 03:25:23 -0800

    Update cleanlonghornorphanedpods.sh

--- cleanlonghornorphanedpods.sh
+++ cleanlonghornorphanedpods.sh
@@ -1,3 +1,4 @@
+#https://github.com/longhorn/longhorn/issues/3207
 while true ; do
 for o in $(tail /var/log/syslog | grep -o  -E 'orphaned pod \\"((\w|-)+)\\' | cut -d" " -f3 | grep -oE '(\w|-)+' | uniq); do
 	p="/var/lib/kubelet/pods/$o/volumes/*"
